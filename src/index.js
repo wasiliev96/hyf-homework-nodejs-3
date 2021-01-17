9a0f8338760f62c31ecff41c391d3d181cd7ae70
wasiliev96 <wasiliev.cloud@gmail.com>
2021-01-17 18:08:06 +0200

    fix routes

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-const users = [{ id: 0 }];
+const users = [];
 app.get("/users", (req, res) => {
   res.json(users);
 });
+app.post("/user", (req, res) => {
+  const newUser = { id: users.length };
+  users.push(newUser);
+  res.status(200).json(newUser);
+});
 app.get(`/user/:id`, (req, res) => {
-  res.json(users.find((user) => (user.id = req.params.id)));
+  res.send(users.find((user) => (user.id === +req.params.id)));
 });
