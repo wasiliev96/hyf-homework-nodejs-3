4633d54bc1cd704123d05b219f5699f4a3691544
wasiliev96 <wasiliev.cloud@gmail.com>
2021-01-17 15:54:15 +0200

    add GET user by id

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+const users = [{ id: 0 }];
 app.get("users", (req, res) => {
-  res.json({});
+  res.json(users);
+});
+app.get(`users/:id`, (req, res) => {
+  res.json(users.find((user) => (user.id = req.params.id)));
 });
