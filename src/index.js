62d5543c6e7696a689080ab315678b82f7ec15d7
wasiliev96 <wasiliev.cloud@gmail.com>
2021-01-17 15:39:32 +0200

    add GET users route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log(`listening on port 3000`);
-})
-app.get('/', (req,res)=>{
-    res.send('Hello World!')
-})
+app.listen(3000, () => {
+  console.log(`listening on port 3000`);
+});
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("users", (req, res) => {
+  res.json({});
+});
